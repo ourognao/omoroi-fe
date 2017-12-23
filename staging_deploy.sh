7846a3cbfc81c9dca8e18c20257d05a6cf55b3ba
Bago <b.ourognao@gmail.com>
2017-12-23 15:26:06 +0900

    master

---
+++ staging_deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#Get the current branch of your branch
+current_branch="$(git symbolic-ref --short -q HEAD)"
+
+echo "########## GIT PULL MASTER ##########"
+git pull origin master
+
+echo "########## GIT STATUS ##########"
+git status
+
+echo "########## GIT PUSH BRANCH TO GITHUB ##########"
+git add -A .
+git commit -m $current_branch
+git push origin $current_branch
+
+#echo "########## HEROKU PUSH BRANCH TO PHOENIX (FRONTEND - STAGING) ##########"
+#git push pms4-staging "$current_branch":master
+
+echo "CURRENT_BRANCH: $current_branch"
+
+echo "##########【成功】ステージングデプロイスクリプトが完了しました ##########"
+
