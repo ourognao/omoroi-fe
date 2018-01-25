1a6423df1a844081a80d6acfb9b0b9318ecaafdd
Bago <b.ourognao@gmail.com>
2018-01-25 23:40:11 +0900

    bago/new/top-page-design

--- containers/top/index/Main.vue
+++ containers/top/index/Main.vue
@@ -1,6 +1,10 @@
 <template lang="pug">
   div#top-index-main(fluid)
-    v-carousel( delimiter-icon="lens" class="hidden-sm-and-down delimiterBackground")
+    v-layout.tabs(row class="hidden-md-only hidden-lg-only hidden-xl-only")
+      v-flex.primary(xs4 class="del") {{ $t('top.tabs.social')}}
+      v-flex.primary(xs4 class="del") {{ $t('top.tabs.language')}}
+      v-flex.primary(xs4) {{ $t('top.tabs.sports')}}
+    v-carousel( delimiter-icon="lens" class="hidden-sm-and-down delimiter-background")
       v-carousel-item(v-for="(picture,i) in pictures" v-bind:src="picture.src" :key="i")
     v-carousel(class="carouselHeight hidden-md-only hidden-lg-only hidden-xl-only" hide-controls)
       v-carousel-item(v-for="(picture,i) in pictures" v-bind:src="picture.src" :key="i")
@@ -11,7 +15,25 @@
 
 <style lang="stylus">
 #top-index-main
-  .delimiterBackground div.carousel__controls
+  .del
+    position: relative
+  .del:after
+    content: ''
+    height: 40%
+    width: 1px
+    position: absolute
+    right: 0
+    top: 12px
+    background-color: #FFF!important
+  .tabs div
+    background-color: red
+    color: #FFF
+    text-align: center
+    font-size: 12px
+    font-weight: 200
+    min-height: 30px
+    padding-top: 9px
+  .delimiter-background div.carousel__controls
     background: none
     transform: scale(0.5, 0.5)
     -ms-transform: scale(0.5, 0.5)
