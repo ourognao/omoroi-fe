e2b9ded91d699b41e294f71d16ff6091644ad617
Bago <b.ourognao@gmail.com>
2018-03-04 17:03:02 +0900

    bago/new/top-page-design

--- containers/layout/default/Footer.vue
+++ containers/layout/default/Footer.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
 #layout-default-footer
-  v-layout.footer.top-arrow(row)
-    v-flex.ma-0(xs12)
+  v-layout.footer-main.top-arrow(row class="hidden-md-only hidden-lg-only hidden-xl-only")
+    v-flex(xs12)
       img(src="/images/top/top-arrow.png" usemap="#top-arrow")
       map(name="top-arrow" id="top-arrow")
         area(shape="rect" :coords="topArrowCoords", href="#" @click.stop.prevent="scrollTop()")
-  v-layout.footer.dotted-background.logo(row class="mb-2")
+  v-layout.footer-main.dotted-background.logo(row)
     v-flex(xs12 class="text-xs-center")
       img.mt-1(src="/images/logo/original-white.png" @click="goto($router, '/')")
-  v-layout.footer.sns(row)
+  v-layout.footer-main.sns(row)
     v-flex.ma-0(xs12 class="text-xs-center black")
       a(href="https://facebook.com/omoroilife" target="_blank")
         img.pointable(src="/images/sns/facebook-mc.png")
@@ -22,7 +22,7 @@
         img.pointable(src="/images/sns/youtube-mc.png")
       a(href="https://www.linkedin.com/company/omoroilife" target="_blank")
         img.pointable(src="/images/sns/linked-in-mc.png")
-  v-layout.footer.dotted-background.copyright(row)
+  v-layout.footer-main.dotted-background.copyright(row)
     v-flex.ma-0(xs12 class="text-xs-center grey-text") {{ $t('top.footer.copyright') }}
 </template>
 
@@ -30,10 +30,13 @@
 
 <style lang="stylus">
 #layout-default-footer
+  .grey-text
+    color #bdbdbd
+
   .pointable
     cursor pointer
 
-  .footer
+  .footer-main
     &.dotted-background
       background-color #212121
       background-image -webkit-repeating-radial-gradient(center center, rgba(0,0,0,.2), rgba(0,0,0,.2) 1px, transparent 1px, transparent 100%)
@@ -45,22 +48,23 @@
       background-size 3px 3px
     &.top-arrow
       area:hover, area:active, area:focus
-          outline: none
-          border: 0
+          outline none
+          border 0
       background none
       img
         max-width 100%
         max-height 100%
     &.logo
-      padding 2em 0 2.5em 0
+      margin-top -4px
+      padding 0.3em 0 0.3em 0
       img
         width 50%
     &.sns
       img
-        width 32px
+        width 24px
         margin 15px 10px 15px 10px
     &.copyright
-      padding 2em 0 1em 0
+      padding 0.3em 0 0.3em 0
 </style>
 
 <!-- ============================================================================ -->
@@ -81,12 +85,10 @@ export default {
 
   methods: {
     scrollTop () {
-      setTimeout(() => {
-        document.documentElement.scroll({
-          top: 0,
-          behavior: 'smooth'
-        })
-      }, 100)
+      document.documentElement.scroll({
+        top: 0,
+        behavior: 'smooth'
+      })
     }
   }
 }
