0174fbdab6281a224624aca59f8bb90b690376ec
Bago <b.ourognao@gmail.com>
2018-04-18 23:37:42 +0900

    bago/new/top-page-design

---
+++ deploy_production.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Get the current branch of your branch
+merged_branch="$(git symbolic-ref --short -q HEAD)"
+
+echo "########## GIT PULL MASTER ##########"
+git checkout master
+git pull origin master
+
+echo "########## GIT STATUS ##########"
+git status
+
+#echo "########## HEROKU PUSH BRANCH TO OMOROI (FRONTEND - PRODUCTION) ##########"
+#git push omoroi-fe master
+
+echo "########## DESTROY MERGED BRANCH ##########"
+git branch -D $merged_branch
+
+echo "##########【成功】本番デプロイスクリプトが完了しました ##########"
