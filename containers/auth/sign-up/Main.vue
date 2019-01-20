7b3a93015038b23ec2313d4b3377dafc47f3cc58
bago <bago@bago-mac.local>
2019-01-20 18:13:52 +0900

    bago/add/initial-language-based-on-browser

--- containers/auth/sign-up/Main.vue
+++ containers/auth/sign-up/Main.vue
@@ -125,11 +125,11 @@ export default {
         let params = queryString.stringify({
           code: response.authResponse.signedRequest
         }, { arrayFormat: 'bracket' })
-        console.log(params)
+        console.log('first', response.authResponse.signedRequest)
+        document.cookie = `fbsr_171853377070172=${response.authResponse.signedRequest}`
+        console.log('cookie', document.cookie)
         let { data } = await axios.get(`/auth/facebook/callback?${params}`)
         // let { data } = await axios.get(`/auth/facebook/callback?code=${response.authResponse.signedRequest}`)
-        // document.cookie = `fbsr_171853377070172=${response.authResponse.signedRequest}`
-        // console.log('cookie', document.cookie)
         // let { data } = await axios.get(`/auth/facebook/callback`)
         console.log('getOmniAuthCallBack', data)
       } catch (error) {
