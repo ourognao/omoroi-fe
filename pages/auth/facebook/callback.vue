667d4eef5f25a5f3178e39a31cc7e687a9640e6b
bago <bago@bago-mac.local>
2019-01-23 21:45:21 +0900

    bago/add/initial-language-based-on-browser

---
+++ pages/auth/facebook/callback.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+#auth-facebook-callback
+  auth-facebook-callback-main
+</template>
+
+<!-- ============================================================================ -->
+
+<script>
+import AuthFacebookCallbackMain from '~/containers/auth/facebook/callback/Main'
+
+export default {
+  components: {
+    AuthFacebookCallbackMain
+  },
+  head () {
+    return {
+      title: `${this.$t('base.head.title')}`
+    }
+  },
+  async asyncData ({ query, route, store, redirect }) {
+    store.commit('merge', ['base.layout', { current: 'auth.facebook.callback', fullPath: route.fullPath }])
+  }
+}
+</script>
